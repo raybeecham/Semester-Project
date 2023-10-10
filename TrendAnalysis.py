07dc73aaf86b09e50fd2313f125a838bd6e3daa2
Ray <raybeecham2009@gmail.com>
2023-10-10 18:10:46 -0500

    refactor and clean

--- TrendAnalysis.py
+++ TrendAnalysis.py
@@ -8,182 +8,178 @@ from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.callbacks import EarlyStopping
 from datetime import datetime
 
-# Download Data From Yahoo Finance
-ticker = "^DJI"
-current_date = datetime.today().strftime('%Y-%m-%d')
-data = yf.download(ticker, start="1992-10-01", end=current_date)
+# Constants
+TICKER = "^DJI"
+START_DATE = "1992-10-01"
+TIME_STEP = 100
+ATR_PERIOD = 141
 
-# Functions
-# Average True Range Calculation
-def average_true_range(data, period=14):
+# Dates and events plotting
+important_dates_and_events = {
+"1999-03-29": "10,000+ Close",
+"2001-09-17": "Reopening after 9/11",
+"2007-07-19": "14,000+ Close",
+"2008-10-13": "Financial Crisis Low",
+"2013-03-05": "All-time High",
+"2020-03-23": "COVID-19 Low"
+}
+
+def fetch_data(ticker, start_date):
+    """Download the stock data from Yahoo Finance."""
+    current_date = datetime.today().strftime('%Y-%m-%d')
+    return yf.download(ticker, start=start_date, end=current_date)
+
+def compute_average_true_range(data, period=ATR_PERIOD):
+    """Calculate Average True Range for volatility analysis."""
     data['H-L'] = abs(data['High'] - data['Low'])
     data['H-PC'] = abs(data['High'] - data['Close'].shift(1))
     data['L-PC'] = abs(data['Low'] - data['Close'].shift(1))
     TR = data[['H-L', 'H-PC', 'L-PC']].max(axis=1)
     return TR.rolling(period).mean()
 
-# Closest Date Calculation
+def calculate_statistics(data):
+    """Compute and print statistics for the dataset."""
+    # Historical Highs and Lows
+    high_date = data['High'].idxmax()
+    low_date = data['Low'].idxmin()
+    print(f"Historical High: {data['High'].max()} on {high_date.strftime('%Y-%m-%d')}")
+    print(f"Historical Low: {data['Low'].min()} on {low_date.strftime('%Y-%m-%d')}")
+    
+    # Statistical Analysis
+    print(f"Mean Closing Price: {data['Close'].mean()}")
+    print(f"Median Closing Price: {data['Close'].median()}")
+    print(f"Standard Deviation of Closing Price: {data['Close'].std()}")
+    print(f"Variance of Closing Price: {data['Close'].var()}")
+    
+    # Return and Drawdown Calculation
+    data['Daily Return'] = data['Close'].pct_change()
+    print(f"Total Return: {(data['Close'].iloc[-1] / data['Close'].iloc[0]) - 1:.2%}")
+    data['Peak'] = data['Close'].cummax()
+    data['Drawdown'] = (data['Close'] - data['Peak']) / data['Peak']
+    print(f"Max Drawdown: {data['Drawdown'].min():.2%}")
+
 def get_closest_date(target_date, available_dates):
     """Get the closest date to the target date from a list of available dates."""
     return min(available_dates, key=lambda d: abs(d - target_date))
 
-# LSTM Data Preparation
+
+def lstm_prediction(prices, time_step=TIME_STEP):
+    """Predict the next closing price using LSTM model."""
+    # Scaling and data preparation
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    prices_scaled = scaler.fit_transform(prices.reshape(-1, 1))
+    X, y = create_dataset(prices_scaled, time_step)
+    X = X.reshape(X.shape[0], X.shape[1], 1)
+    
+    # LSTM Model
+    model = build_lstm_model(time_step)
+    
+    # Train Model
+    train_size = int(len(X) * 0.8)
+    X_train, y_train = X[:train_size], y[:train_size]
+    X_val, y_val = X[train_size:], y[train_size:]
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    model.fit(X_train, y_train, epochs=1, validation_data=(X_val, y_val), callbacks=[early_stopping], verbose=1)
+    
+    # Predict Next Closing Price
+    test_data = prices_scaled[-time_step:].reshape(1, time_step, 1)
+    predicted_price = model.predict(test_data)
+    return scaler.inverse_transform(predicted_price)[0][0]
+
+def build_lstm_model(time_step):
+    """Construct the LSTM model."""
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=(time_step, 1)))
+    model.add(LSTM(50))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
 def create_dataset(data, time_step=1):
+    """Prepare data for LSTM training."""
     dataX, dataY = [], []
     for i in range(len(data) - time_step - 1):
         dataX.append(data[i:(i + time_step)])
         dataY.append(data[i + time_step])
     return np.array(dataX), np.array(dataY)
 
-# LSTM Prediction
-def lstm_prediction(prices):
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    prices_scaled = scaler.fit_transform(prices.reshape(-1, 1))
-    TIME_STEP = 100
-    X, y = create_dataset(prices_scaled, TIME_STEP)
-    X = X.reshape(X.shape[0], X.shape[1], 1)
-    
-    # LSTM Model
-    model = Sequential()
-    model.add(LSTM(50, return_sequences=True, input_shape=(TIME_STEP, 1)))
-    model.add(LSTM(50))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-    
-    # Train Model
-    train_size = int(len(X) * 0.8)
-    X_train, y_train = X[:train_size], y[:train_size]
-    X_val, y_val = X[train_size:], y[train_size:]
-    model.fit(X_train, y_train, epochs=5, validation_data=(X_val, y_val), callbacks=[early_stopping], verbose=1)
-    
-    # Predict Next Closing Price
-    test_data = prices_scaled[-TIME_STEP:].reshape(1, TIME_STEP, 1)
-    predicted_price = model.predict(test_data)
-    predicted_price = scaler.inverse_transform(predicted_price)
-    return predicted_price[0][0]
+def get_next_business_day(last_date):
+    """Determine the next business day after the given date."""
+    next_date = last_date + pd.Timedelta(days=2)
+    while next_date.weekday() > 4:  # 5: Saturday, 6: Sunday
+        next_date += pd.Timedelta(days=1)
+    return next_date
 
-# Volatility Analysis (ATR Calculation)
-data['ATR'] = average_true_range(data)
+def plot_data(data, predicted_date, predicted_next_closing):
+    """Visualize the historical data and predictions."""
+    # Resample to monthly data for plotting
+    monthly_data = data.resample('M').mean()
 
-# Historical Highs and Lows
-high_date = data['High'].idxmax()
-print(f"Historical High: {data['High'].max()} on {high_date.strftime('%Y-%m-%d')}")
-low_date = data['Low'].idxmin()
-print(f"Historical Low: {data['Low'].min()} on {low_date.strftime('%Y-%m-%d')}")
+    plt.figure(figsize=(12, 10))
 
-# Statistical Analysis of the Mean, Median, Standard Deviation, and Variance
-print(f"Mean Closing Price: {data['Close'].mean()}")
-print(f"Median Closing Price: {data['Close'].median()}")
-print(f"Standard Deviation of Closing Price: {data['Close'].std()}")
-print(f"Variance of Closing Price: {data['Close'].var()}")
+    # Subplot 1: Closing Price with Moving Averages
+    ax1 = plt.subplot(3, 1, 1)
+    plt.plot(monthly_data['Close'], label='Closing Price', color='blue')
+    plt.plot(monthly_data['Close'].rolling(window=6).mean(), 'g--', label='6-months SMA')
+    plt.plot(monthly_data['Close'].rolling(window=12).mean(), 'r--', label='12-months SMA')
+    plt.title("Dow Jones Industrial Average Trend Analysis")
+    plt.ylabel("Price")
+    plt.scatter(predicted_date, predicted_next_closing, color='red', label='Predicted Closing Price', zorder=5)
+    annotation_text = f"{predicted_date.strftime('%Y-%m-%d')}: ${predicted_next_closing:.2f}"
+    plt.annotate(annotation_text, (predicted_date, predicted_next_closing), textcoords="offset points", xytext=(0, -15), ha='center', fontsize=8, color='red')
+    plt.legend()
+    plt.grid(True)
 
-# Return Calculation
-data['Daily Return'] = data['Close'].pct_change()
-print(f"Total Return: {(data['Close'].iloc[-1] / data['Close'].iloc[0]) - 1:.2%}")
+    # Dates and events plotting
+    for date, event in important_dates_and_events.items():
+        closest_date = get_closest_date(pd.Timestamp(date), monthly_data.index)
+        ax1.axvline(closest_date, color='k', linestyle='--', alpha=0.7)
+        ax1.annotate(event, (closest_date, monthly_data['Close'][closest_date]), 
+                     textcoords="offset points", xytext=(-0,-30), ha='center', fontsize=5, color='black')
 
-# Drawdown Analysis
-data['Peak'] = data['Close'].cummax()
-data['Drawdown'] = (data['Close'] - data['Peak']) / data['Peak']
-print(f"Max Drawdown: {data['Drawdown'].min():.2%}")
+    # Subplot 2: Volume
+    plt.subplot(3, 1, 2)
+    plt.bar(monthly_data.index, monthly_data['Volume'], color='gray', width=pd.Timedelta(days=20), label='Volume')
+    plt.title("Volume Analysis")
+    plt.ylabel("Volume")
+    plt.legend()
+    plt.grid(True)
+    plt.ticklabel_format(axis="y", style="plain")
 
-# --Only used this to analyze my volume data. Feel free to uncomment and use it.--
-# Volume Analysis
-# volume_stats = data['Volume'].describe()
-# print(volume_stats)
-# high_volume = data['Volume'][data['Volume'] > data['Volume'].quantile(0.95)]
-# low_volume = data['Volume'][data['Volume'] < data['Volume'].quantile(0.05)]
-# print("High Volume Dates:", high_volume)
-# print("Low Volume Dates:", low_volume)
-# correlation = data['Volume'].corr(data['Daily Return'])
-# print("Correlation between Volume and Daily Returns:", correlation)
+    # Subplot 3: ATR - Volatility
+    plt.subplot(3, 1, 3)
+    plt.plot(monthly_data['ATR'], label='Average True Range', color='red')
+    plt.title("Volatility Analysis")
+    plt.xlabel("Date")
+    plt.ylabel("Average True Range(ATR)")
+    plt.legend()
+    plt.grid(True)
 
-# Predict the next closing price while skipping weekends for the predicted date. 
-predicted_next_closing = lstm_prediction(data['Close'].values)
+def main():
+    # 1. Fetch the data
+    data = fetch_data(TICKER, START_DATE)
 
-# Days difference
-days_diff = (pd.Timestamp.today() - data.index[-1]).days
+    # 2. Calculate statistics
+    calculate_statistics(data)
 
-# Predict the next closing price for tomorrow (or next business day).
-predicted_date = data.index[-1] + pd.Timedelta(days=1)
+    # 3. Data Analysis: Add ATR to the data
+    data['ATR'] = compute_average_true_range(data)
 
-# If today's data is already included in the dataset, advance by one day
-if predicted_date == pd.Timestamp.today():
-    predicted_date += pd.Timedelta(days=1)
+    # 4. Make a prediction
+    predicted_next_closing = lstm_prediction(data['Close'].values)
+    predicted_date = get_next_business_day(data.index[-1])
+    print(f"Predicted closing price for {predicted_date.strftime('%Y-%m-%d')}: ${predicted_next_closing:.2f}")
 
-# Skip weekends
-while predicted_date.weekday() > 4:  # 5: Saturday, 6: Sunday
-    predicted_date += pd.Timedelta(days=1)
+    # 5. Resample the data to monthly frequency
+    monthly_data = data.resample('M').mean()
 
-print(f"Predicted closing price for {predicted_date.strftime('%Y-%m-%d')}: ${predicted_next_closing:.2f}")
+    # 6. Plot the data
+    plot_data(data, predicted_date, predicted_next_closing)
 
+    # Adjusting the spacing between subplots
+    plt.subplots_adjust(hspace=0.4)
+    plt.tight_layout()
+    plt.show()
 
-
-# Resample to monthly data for plotting
-monthly_data = data.resample('M').mean()
-
-# Plotting
-plt.figure(figsize=(12, 10))  # Increased the size slightly for clarity
-
-# Subplot 1: Closing Price with Moving Averages
-plt.subplot(3, 1, 1)
-plt.plot(monthly_data['Close'], label='Closing Price', color='blue')
-plt.plot(monthly_data['Close'].rolling(window=6).mean(), 'g--', label='6-months SMA')
-plt.plot(monthly_data['Close'].rolling(window=12).mean(), 'r--', label='12-months SMA')
-plt.title("Dow Jones Industrial Average Trend Analysis")
-plt.ylabel("Price")
-plt.scatter(predicted_date, predicted_next_closing, color='red', label='Predicted Closing Price', zorder=5) 
-annotation_text = f"{predicted_date.strftime('%Y-%m-%d')}: ${predicted_next_closing:.2f}"
-plt.annotate(annotation_text, (predicted_date, predicted_next_closing), textcoords="offset points", xytext=(0,-15), ha='center', fontsize=8, color='red')
-plt.legend()
-plt.grid(True)
-
-# Dates and Events
-important_dates_and_events = {
-    "1999-03-29": "10,000+ Close",
-    "2001-09-17": "Reopening after 9/11",
-    "2007-07-19": "14,000+ Close",
-    "2008-10-13": "Financial Crisis Low",
-    "2013-03-05": "All-time High",
-    "2020-03-23": "COVID-19 Low",
-
-}
-# Plotting the dates and events
-for date, event in important_dates_and_events.items():
-    closest_date = get_closest_date(pd.Timestamp(date), monthly_data.index)
-    plt.axvline(closest_date, color='k', linestyle='--', alpha=0.7)
-    plt.annotate(event, (closest_date, monthly_data['Close'][closest_date]), 
-                 textcoords="offset points", xytext=(-0,-30), ha='center', fontsize=5, color='black')
-
-# Subplot 2: Volume
-plt.subplot(3, 1, 2)
-plt.bar(monthly_data.index, monthly_data['Volume'], color='gray', width=pd.Timedelta(days=20), label='Volume')
-plt.title("Volume Analysis")
-plt.ylabel("Volume")
-plt.legend()
-plt.grid(True)
-plt.ticklabel_format(axis="y", style="plain")
-
-# Subplot 3: ATR - Volatility
-plt.subplot(3, 1, 3)
-plt.plot(monthly_data['ATR'], label='Average True Range', color='red')
-plt.title("Volatility Analysis")
-plt.xlabel("Date")
-plt.ylabel("Average True Range(ATR)")
-plt.legend()
-plt.grid(True)
-
-# --Decided against Dispersion--
-# # Dispersion
-# plt.figure(figsize=(10, 6))
-# plt.hist(data['Close'], bins=50, color='lightblue', edgecolor='black')
-# plt.axvline(data['Close'].mean(), color='red', linestyle='dashed', linewidth=1)
-# plt.title('Distribution of Closing Prices with Mean Indicated')
-# plt.xlabel('Closing Price')
-# plt.ylabel('Frequency')
-# plt.legend(['Mean', 'Closing Price'])
-
-# Adjusting the spacing between subplots
-plt.subplots_adjust(hspace=0.4)
-plt.tight_layout()
-plt.show()
+# Call the main function
+main()
