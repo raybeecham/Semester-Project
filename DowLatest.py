5af0b09693da3c433567d163d83c323edddc7819
Ray <raybeecham2009@gmail.com>
2023-10-11 19:24:27 -0500

    add Dow Jones latest data

---
+++ DowLatest.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+
+# Fetch the Dow Jones Industrial Average data
+ticker_symbol = "^DJI"
+dow_jones = yf.Ticker(ticker_symbol)
+
+# Get the latest historical daily data (you can adjust the period as needed)
+latest_data = dow_jones.history(period="max")
+
+print(latest_data)
+
+# Fetch other relevant information
+info = dow_jones.info
+print("\nDow Jones Information:")
+print(f"Name: {info['shortName']}")
+print(f"Previous Close: {info['previousClose']}")
+print(f"Open: {info['open']}")
+print(f"Volume: {info['volume']}")
+
+# Check if 'marketCap' is in the info dictionary before accessing it
+if 'marketCap' in info:
+    print(f"Market Cap: {info['marketCap']}")
+else:
+    print("Market Cap: N/A")
+
+print(f"52 Week High: {info.get('fiftyTwoWeekHigh', 'N/A')}")
+print(f"52 Week Low: {info.get('fiftyTwoWeekLow', 'N/A')}")
+
+# Convert timezone-aware datetime columns to timezone-naive
+latest_data.index = latest_data.index.tz_localize(None)
+
+# Export to Excel
+latest_data.to_excel("dow_jones_data.xlsx", engine='openpyxl')
